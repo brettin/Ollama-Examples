8dd15db710785ec5790f6d26b09e053fffc75b03
Thomas Brettin <brettin@anl.gov>
2024-01-15 10:08:58 -0600

    sym link /dev/shm

--- env.sh
+++ env.sh
@@ -3,4 +3,12 @@ PATH=$PATH:/rbscratch/brettin/Ollama-Examples/ollama
 PATH=$PATH:/usr/local/go/bin
 PATH=$PATH:.
 
+if [[ -L $HOME/.ollama || -d $HOME/.ollama ]] ; then
+	echo "$HOME/.ollama exists."
+	echo "Can not create sym link."
+	echo "For best performance, models should be staged in /dev/shm/.ollama."
+else
+	ln -s /dev/shm/.ollama $HOME/.ollama
+fi
+
 
